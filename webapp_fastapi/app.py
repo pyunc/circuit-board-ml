4fd4138a8050398b4d6228df25020ac7c8c85ce0
paulo.yun <paulo.yun@ifood.com.br>
2022-11-30 01:09:34 +0000

    batch prediction in dev

--- webapp_fastapi/app.py
+++ webapp_fastapi/app.py
@@ -1,25 +1,25 @@
+import json
 import logging
 import os
 from pathlib import Path
 
 import cv2
-
 import numpy as np
 import tensorflow as tf
 import uvicorn
 from fastapi import FastAPI, Path, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-import cv2
 
 log = logging.getLogger(__name__)
 
 app = FastAPI(
-    title='SMART Data Science Application',
-    description='A Smart Data Science Application running on FastAPI + uvicorn',
-    version='0.0.1'
-)
+    title='Circuit board classifier APP',
+    description='A quality check App running on FastAPI + uvicorn',
+    version='0.0.1')
 
 loaded_model = tf.keras.models.load_model('./saved_model/model.h5')
+
 dictionary = {0: 'adequate', 1: 'inadequate'}
 
 
@@ -29,18 +29,25 @@ def main(file_path):
     files = os.listdir(file_path)
     result_dict = {}
 
-    for file in files[:200]:
+    for file in files[:10]:
 
         img = cv2.imread(os.path.join(file_path, file))
         img = img[..., ::-1]
 
-        df_output = loaded_model.predict(img[None, ...], batch_size=16, verbose=2, steps=None)
+        df_output = loaded_model.predict(img[None, ...], batch_size=1, verbose=2, steps=1)
 
         result = dictionary[np.argmax(df_output[0])]
 
         result_dict[file] = result
 
-    return result_dict
+    # TODO write json in a s3
+
+    json_result = jsonable_encoder(result_dict)
+    out_file = open("result.json", "w", encoding='utf8')
+    json.dump(json_result, out_file, indent=2)
+    out_file.close()
+
+    return JSONResponse(result_dict)
 
 
 if __name__ == "__main__":
